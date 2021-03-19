bdc9091464ad27b60fbb41038cf0ada12f7d1592
Frityet <ambhogal01@gmail.com>
2021-03-19 14:40:32 -0700

    this should work

--- computer/14/Programs/program.lua
+++ computer/14/Programs/program.lua
@@ -2,4 +2,9 @@ local MetalInfuser = peripheral.find("mekanism:metallurgic_infuser");
 
 print("metallurgic_infuser WRAPPED!");
 
-print("Methods:\n\n" .. MetalInfuser)
+
+print("Methods:\n\n")
+
+for i,method in pairs(MetalInfuser) do
+    print(method);
+end
