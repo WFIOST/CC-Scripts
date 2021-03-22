9d549b66c014d5912c024e7d3f2887de4a9248a9
Frityet <ambhogal01@gmail.com>
2021-03-21 19:07:51 -0700

    ef

--- computer/9/recive.lua
+++ computer/9/recive.lua
@@ -12,18 +12,4 @@ modem.open(Ports.TRANSMIT);
 
 local msgArgs = {os.pullEvent("modem_message")}
 
-for k, v in pairs(msgArgs)
-do
-    print(k);
-    print(v);
-    print();
-
-    if k == 5 then
-        for i, j in pairs(v)
-        do
-            print(i);
-            print(j);
-            print();
-        end
-    end
-end
+print(msgArgs[5]);
