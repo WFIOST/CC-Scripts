97da428c439959e00b61556c2594f81f9dbd4ae4
Frityet <ambhogal01@gmail.com>
2021-03-21 18:48:39 -0700

    MODEM SHIT

--- computer/14/Programs/program.lua
+++ computer/14/Programs/program.lua
@@ -1,13 +1,24 @@
-local MetalInfuser = peripheral.find("BigReactors-Reactor");
-
+local Reactor = peripheral.find("BigReactors-Reactor");
 print("Reactor WRAPPED!");
-
+local Modem = peripheral.find("modem");
+print("modem WRAPPED!");
 
 print("Methods:\n\n")
 
-for k, v in pairs(MetalInfuser) do
+for k, v in pairs(Reactor) do
     print(v);
     print("Function name: " .. k);
     print();
 end
 
+local ports = 
+{
+    TRANSMIT = 420,
+    RECIVE = 69
+}
+
+Modem.open(ports.TRANSMIT);
+
+Modem.open(ports.RECIVE);
+
+Modem.transmit(ports.TRANSMIT, ports.RECIVE, "Hello World!");
